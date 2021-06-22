e4428082869e6f123ed5c401ae3ba58c21ca44b8
Aditya <xypnox@gmail.com>
2021-06-22 17:06:58 +0530

    Use slate editor (#7)
    https://github.com/workduck-io/mex-electron/pull/7

--- src/Editor/Editor.tsx
+++ src/Editor/Editor.tsx
@@ -1,40 +1,44 @@
 import linkIcon from '@iconify-icons/ri/link';
 import more2Fill from '@iconify-icons/ri/more-2-fill';
 import shareLine from '@iconify-icons/ri/share-line';
+import saveLine from '@iconify-icons/ri/save-line';
+
+import {
+  createSlatePluginsComponents,
+  createSlatePluginsOptions,
+  SlatePlugins,
+  useStoreEditorValue,
+} from '@udecode/slate-plugins';
 import React, { useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
-import RichMarkdownEditor from 'rich-markdown-editor';
 import { useEditorContext } from '../Context/Editor';
 import IconButton from '../Styled/Buttons';
-import {
-  dark,
-  InfoTools,
-  NodeInfo,
-  NoteTitle,
-  StyledEditor,
-} from '../Styled/Editor';
+import { InfoTools, NodeInfo, NoteTitle, StyledEditor } from '../Styled/Editor';
+
+import { initialValueBasicElements } from './defaultValue';
+import Plugins from './plugins';
+
+const components = createSlatePluginsComponents();
+const options = createSlatePluginsOptions();
 
 const Editor = () => {
-  const noF = () => {};
   const edCtx = useEditorContext();
   useEffect(() => {
     ReactTooltip.rebuild();
   }, []);
   const content = edCtx.state?.content ?? 'Start Writing';
-  const id = edCtx.state?.node.id || '@';
+  const useEditorState = useStoreEditorValue();
+  // const id = edCtx.state?.node.id || '@';
+  const editableProps = {
+    placeholder: 'Type…',
+    style: {
+      padding: '15px',
+    },
+  };
 
-  // console.log('Editor here is updating', { content, id });
-
-  const onClickLink = (href: string, event: unknown) => {
-    console.log('Click', href, event); // eslint-disable-line no-console
-    // currently opens link in the browser, check if
-    window.open(href, '_blank');
-    // require('shell').openExternal('http://www.google.com');
-    // if (isInternalLink(href)) {
-    //   history.push(href);
-    // } else {
-    //   window.location.href = href;
-    // }
+  const onSave = () => {
+    console.log(useEditorState);
+    // On save the editor should serialize the state to markdown plaintext
   };
 
   return (
@@ -42,40 +46,24 @@ const Editor = () => {
       <NodeInfo>
         <NoteTitle>{edCtx?.state?.node.title}</NoteTitle>
         <InfoTools>
+          <IconButton size={24} icon={saveLine} onClick={onSave} title="Save" />
           <IconButton size={24} icon={shareLine} title="Share" />
           <IconButton size={24} icon={linkIcon} title="Copy Link" />
           <IconButton size={24} icon={more2Fill} title="Options" />
         </InfoTools>
       </NodeInfo>
-      <RichMarkdownEditor
-        theme={dark}
-        // Dark theme for the editor
 
-        autoFocus
-        // When set true together with readOnly set to false, focus at the end of the document automatically.
-
-        defaultValue={content}
-        value={content}
-        /** Default value is set for the first render
-         * Value and default value change with editor context
-         * Value is used to change editor content on node change in context
-         */
-
-        id={id}
-        onBlur={noF}
-        onCancel={noF}
-        onClickHashtag={noF}
-        onClickLink={onClickLink}
-        onFocus={noF}
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        onHoverLink={(e: any) => {
-          console.log(e); // eslint-disable-line no-console
-          return false;
-        }}
-        onChange={noF}
-        onSave={noF}
-        onShowToast={noF}
+      <SlatePlugins
+        id="1"
+        editableProps={editableProps}
+        initialValue={initialValueBasicElements}
+        plugins={Plugins}
+        components={components}
+        options={options}
       />
+
+      <hr />
+      <div>{content}</div>
     </StyledEditor>
   );
 };
