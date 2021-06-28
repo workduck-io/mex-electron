0e27a2e58fa85d248427955a40c7373218d6b4bf
xypnox <xypnox@gmail.com>
2021-06-28 18:37:03 +0530

    Fix double link open

--- src/Editor/md-serialize.tsx
+++ src/Editor/md-serialize.tsx
@@ -29,7 +29,7 @@ const customNodeTypes = {
 
 export const deserializationOptions: OptionType = {
   nodeTypes: customNodeTypes,
-  linkDestinationKey: 'src',
+  linkDestinationKey: 'href',
   imageSourceKey: 'src',
   imageCaptionKey: 'alt',
 };
