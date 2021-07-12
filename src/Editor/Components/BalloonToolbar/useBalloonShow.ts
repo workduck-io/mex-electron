f1786a24a33fdd3fc7bb8f6e4e93c8fe24a18bdd
Aditya <xypnox@gmail.com>
2021-07-12 17:07:34 +0530

    Add button toolbar (#10)
    https://github.com/workduck-io/mex-electron/pull/10

---
+++ src/Editor/Components/BalloonToolbar/useBalloonShow.ts
@@ -0,0 +1,90 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useTimeoutFn } from 'react-use';
+import {
+  getSelectionText,
+  isSelectionExpanded,
+} from '@udecode/slate-plugins-common';
+import { TEditor } from '@udecode/slate-plugins-core';
+
+/**
+ * Hide if not selecting.
+ * If hiddenDelay = 0 and the selection changes: show.
+ * If hiddenDelay > 0: hide when the selection length changes.
+ */
+export const useBalloonShow = ({
+  editor,
+  ref,
+  hiddenDelay,
+  selected,
+}: {
+  editor?: TEditor;
+  ref: any;
+  hiddenDelay: number;
+  selected?: boolean;
+}) => {
+  const [hidden, setHidden] = useState(true);
+
+  const selectionExpanded = editor && isSelectionExpanded(editor);
+  const selectionText = editor && getSelectionText(editor);
+
+  const show = useCallback(() => {
+    if (ref.current && hidden && selectionExpanded) {
+      console.log('show: ', { cur: ref.current, hidden, selectionExpanded });
+
+      setHidden(false);
+    }
+  }, [hidden, ref, selectionExpanded]);
+
+  const [, , reset] = useTimeoutFn(show, hiddenDelay);
+
+  useEffect(() => {
+    if (!hiddenDelay) {
+      show();
+    }
+  }, [selectionText?.length, reset, hiddenDelay, show]);
+
+  /**
+   * Hide if not selecting.
+   */
+  useEffect(() => {
+    if (!hidden && !selectionExpanded && !selected) {
+      console.log('Set hidden: ', {
+        cur: ref.current,
+        hidden,
+        selectionExpanded,
+      });
+      setHidden(true);
+      if (ref.current) {
+        ref.current.removeAttribute('style');
+      }
+    }
+  }, [
+    hidden,
+    hiddenDelay,
+    reset,
+    selectionExpanded,
+    selected,
+    show,
+    selectionText?.length,
+    ref,
+  ]);
+
+  // useEffect(() => {
+  //   if (selected) {
+  //     setHidden(() => !selected);
+  //   }
+  // }, [selected]);
+
+  /**
+   * If hiddenDelay > 0:
+   * Hide when the selection length changes.
+   */
+  useEffect(() => {
+    if (!hiddenDelay) return;
+
+    reset();
+    setHidden(true);
+  }, [hiddenDelay, selectionText?.length, reset]);
+
+  return [hidden];
+};
---
+++ src/Editor/Components/BaloonToolbar.tsx
@@ -0,0 +1,237 @@
+import boldIcon from '@iconify-icons/ri/bold';
+import italicIcon from '@iconify-icons/ri/italic';
+import linkIcon from '@iconify-icons/ri/link';
+import underlineIcon from '@iconify-icons/ri/underline';
+import Icon from '@iconify/react';
+import {
+  ELEMENT_LINK,
+  getAbove,
+  getSlatePluginType,
+  isCollapsed,
+  MARK_BOLD,
+  MARK_ITALIC,
+  MARK_UNDERLINE,
+  someNode,
+  ToolbarButtonProps,
+  ToolbarMark,
+  unwrapNodes,
+  upsertLinkAtSelection,
+  useEventEditorId,
+  useStoreEditorRef,
+  useStoreEditorState,
+} from '@udecode/slate-plugins';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Transforms } from 'slate';
+import { ReactEditor } from 'slate-react';
+import styled from 'styled-components';
+import { HeadlessButton } from '../../Styled/Buttons';
+import { BalloonToolbar } from './BalloonToolbar/BalloonToolbar';
+
+const LinkButtonStyled = styled.div`
+  user-select: all;
+  form {
+    display: flex;
+    align-items: center;
+    ${HeadlessButton} {
+      color: inherit;
+    }
+    input {
+      background: ${({ theme }) => theme.colors.background.card};
+      border: 1px solid ${({ theme }) => theme.colors.gray.s3};
+      color: ${({ theme }) => theme.colors.text.secondary};
+      border-radius: ${({ theme }) => theme.borderRadius.tiny};
+    }
+  }
+`;
+
+interface LinkButtonProps extends ToolbarLinkProps {
+  setSelected: (selected: boolean) => void;
+}
+
+const LinkButton = ({ getLinkUrl, setSelected, ...props }: LinkButtonProps) => {
+  const editor = useStoreEditorState(useEventEditorId('focus'));
+
+  const type = getSlatePluginType(editor, ELEMENT_LINK);
+  const isLink = !!editor?.selection && someNode(editor, { match: { type } });
+  const [inp, setInp] = useState({
+    prev: '',
+  });
+
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    // formState: { errors },
+    getValues,
+  } = useForm();
+
+  useEffect(() => {
+    setSelected(true);
+    return () => {
+      setSelected(false);
+    };
+  });
+
+  useEffect(() => {
+    if (!editor) return;
+    const linkNode = getAbove(editor, {
+      match: { type },
+    });
+    if (inp.prev === '' && linkNode) {
+      setInp({
+        prev: linkNode[0].url as string,
+      });
+    }
+  }, [editor, inp.prev, type]);
+
+  const onSubmitLink = async () => {
+    if (!editor) return;
+
+    // Blur focus returns
+    if (!editor || ReactEditor.isFocused(editor)) return;
+    try {
+      ReactEditor.focus(editor);
+      if (!editor.selection && editor.blurSelection) {
+        Transforms.select(editor, editor.blurSelection);
+      }
+    } catch (err) {
+      console.error(err); // eslint-disable-line no-console
+    }
+
+    const linkNode = getAbove(editor, {
+      match: { type },
+    });
+    if (linkNode) {
+      setInp({
+        prev: linkNode[0].url as string,
+      });
+    }
+    console.log(inp);
+
+    let url = '';
+    if (getLinkUrl) {
+      const tempUrl = await getLinkUrl(inp.prev);
+      if (tempUrl) {
+        url = tempUrl;
+      }
+    } else {
+      // Get url from user
+      const val = getValues();
+      // console.log({ val });
+
+      if (val['link-input']) url = val['link-input'];
+    }
+
+    if (inp.prev) {
+      if (linkNode && editor.selection)
+        unwrapNodes(editor, {
+          at: editor.selection,
+          match: { type: getSlatePluginType(editor, ELEMENT_LINK) },
+        });
+
+      return;
+    }
+
+    // If our cursor is in middle of a link, then we don't want to inser it inline
+    const shouldWrap: boolean =
+      linkNode !== undefined && isCollapsed(editor.selection);
+    upsertLinkAtSelection(editor, { url, wrap: shouldWrap });
+
+    setSelected(false);
+  };
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+    await onSubmitLink();
+  };
+
+  const { icon } = props;
+
+  return (
+    <LinkButtonStyled className="button_of_link">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <HeadlessButton
+          active={isLink.toString()}
+          // onMouseDown={handleMouseDownLink}
+          type="submit"
+          // tooltip={{ interactive: true }}
+          {...props}
+        >
+          {icon}
+        </HeadlessButton>
+        <input
+          defaultValue={inp.prev}
+          type="text"
+          {...register('link-input')}
+        />
+      </form>
+    </LinkButtonStyled>
+  );
+};
+
+const BallonToolbarMarks = () => {
+  const editor = useStoreEditorRef(useEventEditorId('focus'));
+  const [selected, setSelected] = useState(false);
+
+  const arrow = true;
+  const theme = 'dark';
+  const direction = 'top';
+  const hiddenDelay = 0;
+  const tooltip = {
+    arrow: true,
+    delay: 0,
+    duration: [200, 0] as [number, number],
+    // hideOnClick: false,
+    offset: [0, 17] as [number, number],
+    placement: 'top' as const,
+  };
+  // const styles = {
+  //   root: [
+  //     'slate-BalloonToolbar',
+  //     {
+  //       userSelect: 'all',
+  //     },
+  //   ],
+  // };
+
+  return (
+    <BalloonToolbar
+      direction={direction}
+      hiddenDelay={hiddenDelay}
+      theme={theme}
+      arrow={arrow}
+      // styles={styles}
+      selected={selected}
+    >
+      <ToolbarMark
+        type={getSlatePluginType(editor, MARK_BOLD)}
+        icon={<Icon height={20} icon={boldIcon} />}
+        tooltip={{ content: 'Bold (⌘B)', ...tooltip }}
+      />
+      <ToolbarMark
+        type={getSlatePluginType(editor, MARK_ITALIC)}
+        icon={<Icon height={20} icon={italicIcon} />}
+        tooltip={{ content: 'Italic (⌘I)', ...tooltip }}
+      />
+      <ToolbarMark
+        type={getSlatePluginType(editor, MARK_UNDERLINE)}
+        icon={<Icon height={20} icon={underlineIcon} />}
+        tooltip={{ content: 'Underline (⌘U)', ...tooltip }}
+      />
+      <LinkButton
+        tooltip={{ content: 'Link', ...tooltip }}
+        icon={<Icon height={20} icon={linkIcon} />}
+        setSelected={setSelected}
+      />
+    </BalloonToolbar>
+  );
+};
+
+export interface ToolbarLinkProps extends ToolbarButtonProps {
+  /**
+   * Default onMouseDown is getting the link url by calling this promise before inserting the image.
+   */
+  getLinkUrl?: (prevUrl: string | null) => Promise<string | null>;
+}
+export default BallonToolbarMarks;
