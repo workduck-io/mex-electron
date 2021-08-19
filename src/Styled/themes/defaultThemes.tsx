437110fcb7664e21775ef1ea436196c7ca77e4f5
xypnox <xypnox@gmail.com>
2021-08-19 21:46:49 +0530

    Gruvbox theme corrected and refined

--- src/Styled/themes/defaultThemes.tsx
+++ src/Styled/themes/defaultThemes.tsx
@@ -1,9 +1,11 @@
 import { devTheme } from './devTheme';
 import { dnshTheme } from './dnshTheme';
+import { gruvboxTheme } from './gruvboxTheme';
 import { lightTheme } from './lightTheme';
 import { vvkTheme } from './vvkTheme';
 
 export const defaultThemes = [
+  { id: 'gruvy', themeData: gruvboxTheme },
   { id: 'dev', themeData: devTheme },
   { id: 'light', themeData: lightTheme },
   { id: 'vvk', themeData: vvkTheme },
