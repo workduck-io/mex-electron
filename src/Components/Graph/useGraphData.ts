7411d9878bf036e823a8618dc4ff8f5637ee471b
xypnox <xypnox@gmail.com>
2021-08-25 19:18:13 +0530

    Restyle Graph and links

--- src/Components/Graph/useGraphData.ts
+++ src/Components/Graph/useGraphData.ts
@@ -38,28 +38,33 @@ interface GraphEdge {
   physics?: boolean
 }
 
+const getEx = (level: number) => 1 - Math.pow(Math.E, -level / 4)
+
 const getNodeStyles = (level: number, theme: DefaultTheme) => {
   const { gray, primary } = theme.colors
-
-  const step = 0.3
 
   const colorBase = mix(0.15, gray[3], mix(0.9, gray[10], primary))
   const fontColorBase = mix(0.75, gray[2], primary)
 
-  const color = mix(level * step, gray[10], colorBase)
-  const fontColor = mix((level * step) / 2, gray[8], fontColorBase)
+  const color = mix(getEx(level), gray[10], colorBase)
+  const fontColor = mix(getEx(level), gray[8], fontColorBase)
 
   return {
     color: {
       border: color,
       background: color,
       highlight: {
-        border: mix(0.1, primary, color),
+        border: mix(0.5, primary, color),
         background: mix(0.2, primary, color)
       },
       hover: {
         border: mix(0.2, primary, color),
         background: mix(0.3, primary, color)
+      }
+    },
+    chosen: {
+      label: (values) => {
+        values.color = gray[2]
       }
     },
     font: {
@@ -104,7 +109,7 @@ export const useGraphData = () => {
     const level = getLevel(node)
     return {
       id: id + 1,
-      label: getNodeIdLast(node),
+      label: showLocal ? node : getNodeIdLast(node),
       nodeId: node,
       ...getNodeStyles(level, theme)
     }
