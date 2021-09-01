65feaea7ac210aed356b28419201b48687542945
Dinesh Singh <dinesh@workduck.io>
2021-09-01 21:42:00 +0530

    Sync search results

--- src/Spotlight/components/Content/index.tsx
+++ src/Spotlight/components/Content/index.tsx
@@ -9,6 +9,7 @@ import SideBar from '../SideBar'
 import { useContentStore } from '../../../Editor/Store/ContentStore'
 import { useEditorStore } from '../../../Editor/Store/EditorStore'
 import { getNewDraftKey } from '../../../Editor/Components/SyncBlock/getNewBlockData'
+import useDataStore from '../../../Editor/Store/DataStore'
 
 export const StyledContent = styled.section`
   display: flex;
@@ -26,6 +27,7 @@ const Content = () => {
   const { search, selection, localData } = useSpotlightContext()
 
   const [data, setData] = useState<Array<any>>()
+  const ilinks = useDataStore((s) => s.ilinks)
   const draftKey = useMemo(() => getNewDraftKey(), [])
 
   const [preview, setPreview] = useState<any>(initPreview)
@@ -41,29 +43,27 @@ const Content = () => {
   }, [selection])
 
   useEffect(() => {
-    if (localData) {
-      const results = getSearchResults(search, localData?.ilinks, { keySelector: (obj) => obj.key })
-      if (search) {
-        const resultsWithContent = results.map((result) => {
-          const content = getContent(result.key)
-          let rawText = ''
+    const results = getSearchResults(search, ilinks, { keySelector: (obj) => obj.key })
+    if (search) {
+      const resultsWithContent = results.map((result) => {
+        const content = getContent(result.key)
+        let rawText = ''
 
-          content?.content.map((item) => {
-            rawText += item?.children?.[0]?.text
-            return item
-          })
-
-          return {
-            ...result,
-            desc: rawText
-          }
+        content?.content.map((item) => {
+          rawText += item?.children?.[0]?.text
+          return item
         })
-        setData(resultsWithContent)
-      } else {
-        setData(undefined)
-      }
+
+        return {
+          ...result,
+          desc: rawText
+        }
+      })
+      setData(resultsWithContent)
+    } else {
+      setData(undefined)
     }
-  }, [search])
+  }, [search, ilinks])
 
   useEffect(() => {
     const prevTemplate = {
