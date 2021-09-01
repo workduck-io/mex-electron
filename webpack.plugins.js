5be4542840a00d9621f5a79c887fa11830df7120
rpPanda <ruddhi@workduck.io>
2021-09-01 13:14:18 +0530

    packaging fixes and description updates

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -2,8 +2,4 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
-module.exports = [
-  new ForkTsCheckerWebpackPlugin(),
-  new webpack.ExternalsPlugin('commonjs', ['electron']),
-  new webpack.ExternalsPlugin('commonjs', ['activ-win']),
-]
+module.exports = [new ForkTsCheckerWebpackPlugin(), new webpack.ExternalsPlugin('commonjs', ['electron'])]
