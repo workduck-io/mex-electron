4a14471c81cbf543355a5cc9189f0ce27ff571e9
xypnox <xypnox@gmail.com>
2021-09-07 19:31:09 +0530

    Make a functional google search!

--- src/Components/NodeSelect/NodeSelect.tsx
+++ src/Components/NodeSelect/NodeSelect.tsx
@@ -1,38 +1,10 @@
-import React, { useEffect, useState } from 'react'
 import { useCombobox } from 'downshift'
-import { StyledCombobox, StyledInputWrapper, StyledMenu, Suggestion } from './NodeSelect.styles'
-import { Input } from '../../Styled/Form'
+import React, { useEffect } from 'react'
+import create from 'zustand'
 import useDataStore from '../../Editor/Store/DataStore'
 import { useEditorStore } from '../../Editor/Store/EditorStore'
-// import {items, menuStyles, comboboxStyles} from '../../shared'
-
-function stateReducer (state, actionAndChanges) {
-  const { type, changes } = actionAndChanges
-  // returning an uppercased version of the item string.
-  switch (type) {
-    case useCombobox.stateChangeTypes.InputChange:
-      return {
-        // return normal changes.
-        ...changes,
-        // but taking the change from default reducer and uppercasing it.
-        inputValue: changes.inputValue.toUpperCase()
-      }
-    // also on selection.
-    case useCombobox.stateChangeTypes.ItemClick:
-    case useCombobox.stateChangeTypes.InputKeyDownEnter:
-    case useCombobox.stateChangeTypes.InputBlur:
-      return {
-        ...changes,
-        // if we had an item selected.
-        ...(changes.selectedItem && {
-          // we will show it uppercased.
-          inputValue: changes.inputValue.toUpperCase()
-        })
-      }
-    default:
-      return changes // otherwise business as usual.
-  }
-}
+import { Input } from '../../Styled/Form'
+import { StyledCombobox, StyledInputWrapper, StyledMenu, Suggestion } from './NodeSelect.styles'
 
 type ComboItem = {
   text: string
@@ -43,22 +15,61 @@ interface NodeSelectProps {
   handleSelectItem: (nodeId: string) => void
 }
 
+interface NodeSelectState {
+  inputItems: ComboItem[]
+  selectedItem: ComboItem | null
+
+  setInputItems: (inputItems: ComboItem[]) => void
+  setSelectedItem: (selectedItem: ComboItem | null) => void
+  reset: () => void
+}
+
+const useNodeSelectStore = create<NodeSelectState>((set) => ({
+  inputItems: [],
+  selectedItem: null,
+
+  setInputItems: (inputItems: ComboItem[]) => set({ inputItems }),
+
+  setSelectedItem: (selectedItem: ComboItem | null) => set({ selectedItem }),
+
+  reset: () =>
+    set({
+      inputItems: [],
+      selectedItem: null
+    })
+}))
+
 export function NodeSelect ({ handleSelectItem }: NodeSelectProps) {
-  const ilinks = useDataStore((store) => store.ilinks)
+  const ilinks = useDataStore((store) => store.ilinks).map((l) => ({
+    text: l.key,
+    value: l.key
+  }))
   const loadNodeFromId = useEditorStore((store) => store.loadNodeFromId)
 
-  // Items to display in the combobox suggestions
-  const [inputItems, setInputItems] = useState(ilinks)
-  const [selectedItem, setSelectedItem] = useState(null)
+  const inputItems = useNodeSelectStore((store) => store.inputItems)
+  const setInputItems = useNodeSelectStore((store) => store.setInputItems)
+
+  const selectedItem = useNodeSelectStore((store) => store.selectedItem)
+  const setSelectedItem = useNodeSelectStore((store) => store.setSelectedItem)
+
+  const reset = useNodeSelectStore((store) => store.reset)
+
+  const getNewItems = (inputValue: string) => {
+    const newItems = ilinks.filter((item) => item.text.toLowerCase().startsWith(inputValue.toLowerCase()))
+    if (inputValue !== '' && isNew(inputValue, ilinks)) {
+      newItems.push({ text: `Create new: ${inputValue}`, value: inputValue })
+    }
+    return newItems
+  }
 
   const {
     isOpen,
     getToggleButtonProps,
-    getLabelProps,
     getMenuProps,
     getInputProps,
     getComboboxProps,
     highlightedIndex,
+    setInputValue,
     getItemProps,
     toggleMenu
   } = useCombobox({
@@ -66,32 +77,53 @@ export function NodeSelect ({ handleSelectItem }: NodeSelectProps) {
     selectedItem,
     initialIsOpen: true,
     onSelectedItemChange: handleSelectedItemChange,
-    onInputValueChange: ({ inputValue }) => {
-      const newItems = ilinks.filter((item) => item.text.toLowerCase().startsWith(inputValue.toLowerCase()))
-      if (isNew(inputValue, ilinks)) {
-        console.log('isnew', { inputValue })
-
-        newItems.push({ text: `Create new: ${inputValue}`, value: inputValue, key: '__create_new' })
+    onHighlightedIndexChange: ({ highlightedIndex }) => {
+      const highlightedItem = inputItems[highlightedIndex]
+      if (highlightedItem && highlightedItem.value && highlightedItem.value !== '__create_new') {
+        setInputValue(highlightedItem.value)
       }
-      setInputItems(newItems)
     }
   })
 
-  console.log('ComboMain', { ilinks })
-
   function handleSelectedItemChange ({ selectedItem }: any) {
-    console.log({ selectedItem })
-    setSelectedItem(selectedItem)
+    // console.log({ selectedItem })
+    setSelectedItem(selectedItem.value)
     loadNodeFromId(selectedItem.value)
     handleSelectItem(selectedItem.value)
     toggleMenu()
   }
 
+  const onInpChange = (e) => {
+    // console.log(e)
+    const newItems = getNewItems(e.target.value)
+    setInputItems(newItems)
+  }
+
+  const onKeyUp = (event) => {
+    if (event.key === 'Enter') {
+      if (inputItems[0] && highlightedIndex < 0) {
+        handleSelectItem(inputItems[0].value)
+      }
+    }
+  }
+  useEffect(() => {
+    setInputItems(ilinks)
+    return () => {
+      reset()
+    }
+  }, [])
+
   return (
     <StyledInputWrapper>
-      {/* <label {...getLabelProps()}>Choose an element:</label> */}
       <StyledCombobox {...getComboboxProps()}>
-        <Input {...getInputProps()} />
+        <Input
+          {...getInputProps()}
+          onChange={(e) => {
+            onInpChange(e)
+            getInputProps().onChange(e)
+          }}
+          onKeyUp={onKeyUp}
+        />
         <button type="button" {...getToggleButtonProps()} aria-label="toggle menu">
           &#8595;
         </button>
