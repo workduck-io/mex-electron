7ae13670c4f5bdb13ad14f2a9080639d3833d9d5
xypnox <xypnox@gmail.com>
2021-09-09 18:06:41 +0530

    QuickFix - remove unnecessary IPC listener event mounting

--- src/Data/useLocalData.ts
+++ src/Data/useLocalData.ts
@@ -1,13 +1,17 @@
 import { ipcRenderer } from 'electron'
 import { FileData } from '../Types/data'
 
-export const useLocalData = async () => {
-  const prom = new Promise<FileData>((resolve) => {
-    ipcRenderer.on('recieve-local-data', (_event, arg: FileData) => {
-      resolve(arg)
+export const useLocalData = () => {
+  const getLocalData = async () => {
+    const prom = new Promise<FileData>((resolve) => {
+      ipcRenderer.on('recieve-local-data', (_event, arg: FileData) => {
+        resolve(arg)
+      })
     })
-  })
 
-  ipcRenderer.send('get-local-data')
-  return prom
+    ipcRenderer.send('get-local-data')
+
+    return prom
+  }
+  return { getLocalData }
 }
