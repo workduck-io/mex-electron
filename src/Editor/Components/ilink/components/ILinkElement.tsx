7394036703ac59177a846918ab6901ca75473395
xypnox <xypnox@gmail.com>
2021-09-17 17:04:04 +0530

    Remove preview

--- src/Editor/Components/ilink/components/ILinkElement.tsx
+++ src/Editor/Components/ilink/components/ILinkElement.tsx
@@ -45,13 +45,13 @@ export const ILinkElement = ({ attributes, children, element }: ILinkElementProp
 
   return (
     <SILinkRoot {...attributes} id={`ILINK_${element.value}`} data-slate-value={element.value} contentEditable={false}>
-      <EditorPreview id={element.value}>
-        <SILink focused={selected} {...onClickProps}>
-          <span className="ILink_decoration ILink_decoration_left">[[</span>
-          {element.value}
-          <span className="ILink_decoration ILink_decoration_right">]]</span>
-        </SILink>
-      </EditorPreview>
+      {/* <EditorPreview id={element.value}> */}
+      <SILink focused={selected} {...onClickProps}>
+        <span className="ILink_decoration ILink_decoration_left">[[</span>
+        {element.value}
+        <span className="ILink_decoration ILink_decoration_right">]]</span>
+      </SILink>
+      {/* </EditorPreview> */}
       {children}
     </SILinkRoot>
   )
