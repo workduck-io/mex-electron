55e6265d3483fb6bcc756b40ccbaae10276b127a
xypnox <xypnox@gmail.com>
2021-09-17 21:54:20 +0530

    Stylize the preview

--- src/Editor/Components/EditorPreview/EditorPreview.styles.tsx
+++ src/Editor/Components/EditorPreview/EditorPreview.styles.tsx
@@ -1,9 +1,13 @@
 import { CardShadow } from '../../../Styled/helpers'
 import styled from 'styled-components'
+import { mix, transparentize } from 'polished'
 
 export const EditorPreviewWrapper = styled.div`
   padding: ${({ theme }) => theme.spacing.small};
-  background: ${({ theme }) => theme.colors.gray[9]} !important;
+  background: ${({ theme }) => transparentize(0.5, theme.colors.gray[9])} !important;
+
+  backdrop-filter: blur(8px);
+
   border-radius: ${({ theme }) => theme.borderRadius.small};
   color: ${({ theme }) => theme.colors.fade};
   max-height: 400px;
