8f20ef086e58417710570eaf8d6ec48196e5ce8e
rpPanda <ruddhi@workduck.io>
2021-11-06 13:46:55 +0530

    URL fixes

--- src/Requests/routes.ts
+++ src/Requests/routes.ts
@@ -1,5 +1,4 @@
-export const BASE_INTEGRATION_URL = 'https://api.workduck.io/integration'
-// export const BASE_INTEGRATION_URL = 'https://1873-106-200-232-245.ngrok.io/local'
+export const BASE_INTEGRATION_URL = 'https://http.workduck.io/integration'
 
 export const integrationURLs = {
   createTemplate: `${BASE_INTEGRATION_URL}/sync/template`,
