70069f22253bc5bc39f04bae264553196c034b6e
Mukul Mehta <hello@mukuk-mehta.in>
2021-11-09 07:10:31 +0530

    Selection instead of CLipboard?

--- src/Spotlight/utils/getSelectedText.ts
+++ src/Spotlight/utils/getSelectedText.ts
@@ -11,18 +11,23 @@ export type SelectionType = {
 export const simulateCopy = () => keyTap('c', process.platform === 'darwin' ? 'command' : 'control')
 
 export const getSelectedText = async (): Promise<SelectionType> => {
-  const contentBackup = clipboard.readText()
+  const contentBackup = clipboard.readText('selection')
+  console.log('Read Content as: ', contentBackup)
   clipboard.clear()
   simulateCopy()
   const windowDetails = await activeWindow()
 
   const selectedText = clipboard.readHTML()
   clipboard.writeText(contentBackup)
+  console.log('selected text: ', selectedText)
 
-  return {
+  const ret = {
     text: selectedText,
     metadata: windowDetails
   }
+  console.log('Returning: ', ret)
+  console.log('\n\n')
+  return ret
 }
 
 export const getGlobalShortcut = (shortcut: string) => {
