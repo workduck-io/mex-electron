43371f17f1ee07904d57225bf2b6895abce78ce8
Mukul Mehta <hello@mukul-mehta.in>
2021-11-11 11:16:25 +0530

    hardcode apple id password for now

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -9,7 +9,7 @@ module.exports = {
     rules: require('./webpack.rules')
   },
   externals: {
-    'active-win-universal': 'commonjs2 active-win-universal'
+    'active-win': 'commonjs2 active-win'
   },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
