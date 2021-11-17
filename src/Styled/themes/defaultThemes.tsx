4c388520d9ff3f0e09689197a236f3b8a6ee82dc
xypnox <xypnox@gmail.com>
2021-11-17 22:07:23 +0530

    Rename themes, add new theme Sapphire

--- src/Styled/themes/defaultThemes.tsx
+++ src/Styled/themes/defaultThemes.tsx
@@ -1,21 +1,23 @@
-import { airTheme } from './airTheme'
+// import { airTheme } from './airTheme'
 import { devTheme } from './devTheme'
-import { spotlightTheme } from './spotlightTheme'
+import { gruvboxTheme } from './gruvboxTheme'
 import { lightTheme } from './lightTheme'
 import { matrixTheme } from './matrixTheme'
 import { reduxTheme } from './reduxTheme'
+import { sapphireTheme } from './sapphireTheme'
+import { spotlightTheme } from './spotlightTheme'
 import { vvkTheme } from './vvkTheme'
-import { gruvboxTheme } from './gruvboxTheme'
 
 export const defaultThemes = [
-  { id: 'gruvy', themeData: gruvboxTheme },
-  { id: 'redux', themeData: reduxTheme },
-  { id: 'dev', themeData: devTheme },
-  { id: 'matrix', themeData: matrixTheme },
-  { id: 'light', themeData: lightTheme },
-  { id: 'air', themeData: airTheme },
-  { id: 'vvk', themeData: vvkTheme },
-  { id: 'spotlight', themeData: spotlightTheme }
+  { id: 'Gruvbox', themeData: gruvboxTheme },
+  { id: 'Gruvbox+', themeData: reduxTheme },
+  { id: 'Dev', themeData: devTheme },
+  { id: 'Dev+', themeData: matrixTheme },
+  { id: 'Amethyst', themeData: vvkTheme },
+  { id: 'Light', themeData: lightTheme },
+  { id: 'Sapphire', themeData: sapphireTheme },
+  // { id: '', themeData: airTheme },
+  { id: 'Clean', themeData: spotlightTheme }
 ]
 
 export const getTheme = (themeId: string) => {
