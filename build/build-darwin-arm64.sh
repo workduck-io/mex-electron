034d538a9973572460e67d61cf50eba9c7b8c046
Mukul Mehta <hello@mukul-mehta.in>
2021-11-22 09:56:20 +0530

    Create zip of .app bundle for auto-updater

--- build/build-darwin-arm64.sh
+++ build/build-darwin-arm64.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
+./node_modules/.bin/electron-rebuild --force --arch=arm64
 yarn package --arch=arm64
 
 
 node build/darwin-sign.js arm64
 node build/darwin-notarize.js arm64
 node build/darwin-build-dmg.js arm64
+node build/darwin-build-zip.js arm64
