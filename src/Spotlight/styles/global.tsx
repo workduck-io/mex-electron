d206f60672deccc2c60a631f41aa410f112996b2
xypnox <xypnox@gmail.com>
2021-12-25 01:05:35 +0530

    Add tippy styling to spotlight

--- src/Spotlight/styles/global.tsx
+++ src/Spotlight/styles/global.tsx
@@ -1,3 +1,4 @@
+import { TippyBalloonStyles } from '../../Styled/Toolbar'
 import { createGlobalStyle } from 'styled-components'
 import normalize from './normalize'
 
@@ -5,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
   ${normalize}
 
   html {
-    height: 100vh;  
+    height: 100vh;
     overflow: hidden;
   }
 
@@ -13,6 +14,8 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${({ theme }) => theme.colors.gray[8]};
     font-family: Inter, -apple-system, system-ui, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   }
+
+  ${TippyBalloonStyles}
 
 `
 
