90fac8c0af9df8f137820a832a5d646ab9587f3b
Mukul Mehta <hello@mukul-mehta.in>
2021-12-25 06:22:47 +0530

    Backup mex.json before an update is installed

--- src/Defaults/data.ts
+++ src/Defaults/data.ts
@@ -2,6 +2,8 @@ import { App } from 'electron'
 import path from 'path'
 
 export const DataFileName = 'mex.json'
+export const BackupFolderName = 'backups'
 
 export const getSearchIndexLocation = (app: App) => path.join(app.getPath('userData'), 'search_index')
 export const getSaveLocation = (app: App) => path.join(app.getPath('userData'), DataFileName)
+export const getBackupLocation = (app: App) => path.join(app.getPath('userData'), BackupFolderName)
