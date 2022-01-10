cee9a9218c6fabd1eabfe0741ef4ada70c2f4c75
xypnox <xypnox@gmail.com>
2022-01-10 16:46:28 +0530

    Add react-error-boundary to Editor to catch all editor errors

--- src/Views/EditorView.tsx
+++ src/Views/EditorView.tsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 import SideBar from '../Components/Sidebar'
 import ContentEditor from '../Editor/ContentEditor'
 import { useTreeFromLinks } from '../Editor/Store/DataStore'
+import { useEditorStore } from '../Editor/Store/EditorStore'
 import InfoBar from '../Layout/InfoBar'
+import { ErrorBoundary } from 'react-error-boundary'
+import useLoad from '../Hooks/useLoad/useLoad'
+import EditorErrorFallback from '../Components/Error/EditorErrorFallback'
 
 const EditorViewWrapper = styled.div`
   display: flex;
@@ -14,11 +18,19 @@ const EditorViewWrapper = styled.div`
 
 const EditorView = () => {
   const Tree = useTreeFromLinks()
+  const { loadNode } = useLoad()
+  const node = useEditorStore((s) => s.node)
+
+  const resetEditor = () => {
+    loadNode(node.uid, { fetch: false, savePrev: false })
+  }
 
   return (
     <EditorViewWrapper>
       <SideBar tree={Tree} starred={Tree} />
-      <ContentEditor />
+      <ErrorBoundary onReset={resetEditor} FallbackComponent={EditorErrorFallback}>
+        <ContentEditor />
+      </ErrorBoundary>
       <InfoBar />
     </EditorViewWrapper>
   )
