670d54bd0fd54e3c598e30e330a8ef050a296d41
xypnox <xypnox@gmail.com>
2022-02-16 17:44:41 +0530

    Remove unused lib, and old code

--- src/editor/Components/BalloonToolbar/BalloonToolbar.tsx
+++ src/editor/Components/BalloonToolbar/BalloonToolbar.tsx
@@ -7,47 +7,6 @@ import { BalloonToolbarBase, getBalloonToolbarStyles } from './BalloonToolbar.st
 import { BalloonToolbarProps } from './BalloonToolbar.types'
 import { useBalloonToolbarPopper } from './useBalloonToolbarPopper'
 
-/*
-export const BalloonToolbar = withEditor((props: BalloonToolbarProps) => {
-  const { children, theme = 'dark', arrow = false, portalElement, popperOptions: _popperOptions = {} } = props
-
-  const popperRef = useRef<HTMLDivElement>(null)
-
-  const popperOptions: UsePopperPositionOptions = {
-    popperElement: popperRef.current,
-    placement: 'top' as any,
-    offset: [0, 8],
-    ..._popperOptions
-  }
-
-  const { styles: popperStyles, attributes } = useBalloonToolbarPopper(popperOptions)
-
-  const styles = getBalloonToolbarStyles({
-    popperOptions,
-    theme,
-    arrow,
-    ...props
-  })
-
-  // mog('Balloon', { styles, popperOptions })
-
-  return (
-    <PortalBody element={portalElement}>
-      <BalloonToolbarBase
-        ref={popperRef}
-        css={styles.root.css}
-        className={styles.root.className}
-        style={popperStyles.popper}
-        {...attributes.popper}
-      >
-        {children}
-      </BalloonToolbarBase>
-    </PortalBody>
-
-  )
-})
-    */
-
 export const BalloonToolbar = withPlateProvider((props: BalloonToolbarProps) => {
   const { children, theme = 'dark', arrow = false, portalElement, popperOptions: _popperOptions = {} } = props
 
