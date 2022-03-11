9a21e304e983bdffeea0b3358f25a17c68c9cdf3
xypnox <xypnox@gmail.com>
2022-03-11 19:26:43 +0530

    Fix refactor modal bug

--- src/components/mex/Refactor/Refactor.tsx
+++ src/components/mex/Refactor/Refactor.tsx
@@ -12,6 +12,7 @@ import { useEditorStore } from '../../../store/useEditorStore'
 import { useHelpStore } from '../../../store/useHelpStore'
 import { Button } from '../../../style/Buttons'
 import { NodeLink } from '../../../types/relations'
+import { mog } from '../../../utils/lib/helper'
 import { isReserved } from '../../../utils/lib/paths'
 import { QuickLink, WrappedNodeSelect } from '../NodeSelect/NodeSelect'
 import { doesLinkRemain } from './doesLinkRemain'
@@ -123,11 +124,12 @@ const Refactor = () => {
   const { getUidFromNodeId } = useLinks()
 
   useEffect(() => {
+    mog('Refactor', { open, to, from })
     if (to && from && !isReserved(from) && !isReserved(to)) {
       // mog('To, from in refactor', { to, from })
       setMockRefactored(getMockRefactor(from, to))
     }
-  }, [to, from])
+  }, [to, from, open])
 
   // console.log({ mockRefactored });
 
@@ -144,6 +146,7 @@ const Refactor = () => {
     closeModal()
   }
 
+  mog('Refactor', { open, focus, to, from, mockRefactored })
   return (
     <Modal className="ModalContent" overlayClassName="ModalOverlay" onRequestClose={closeModal} isOpen={open}>
       <ModalHeader>Refactor</ModalHeader>
@@ -167,6 +170,7 @@ const Refactor = () => {
         createAtTop
         disallowClash
         iconHighlight={to !== undefined}
+        defaultValue={to ?? ''}
         handleSelectItem={handleToChange}
         handleCreateItem={handleToCreate}
       />
