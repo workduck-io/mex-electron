30425d0b62f761e8a558b3f435ef8281e97bf89b
Mukul Mehta <hello@mukul-mehta.in>
2022-03-21 01:55:33 +0530

    Compress image using the compress.js library

--- src/utils/imageUpload.ts
+++ src/utils/imageUpload.ts
@@ -1,15 +1,33 @@
 import { client } from '@workduck-io/dwindle'
+import Compress from 'compress.js'
+
+import { mog } from './lib/helper'
 import { apiURLs } from '../apis/routes'
 
 export const uploadImageToWDCDN = async (data: string | ArrayBuffer): Promise<string | ArrayBuffer> => {
   if (typeof data === 'string') {
-    const parsedImage = data.split(',')[1]
-    const resp = await client.post(apiURLs.createImageURL, {
-      encodedString: parsedImage
-    })
-    const path = await resp.data
-    const publicURL = apiURLs.getImagePublicURL(path)
-    return publicURL
+    try {
+      const compress = new Compress()
+      const parsedImage = data.split(',')[1]
+
+      const file = Compress.convertBase64ToFile(parsedImage)
+
+      const compressedImg = (
+        await compress.compress([file], {
+          size: 4,
+          quality: 0.9
+        })
+      )[0]
+
+      const resp = await client.post(apiURLs.createImageURL, {
+        encodedString: compressedImg.data
+      })
+      const path = await resp.data
+      return apiURLs.getImagePublicURL(path)
+    } catch (error) {
+      mog('UploadImageFailed', { error })
+      return data
+    }
   } else {
     return data
   }
