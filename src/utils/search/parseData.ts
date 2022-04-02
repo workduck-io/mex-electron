bc5ccba8bdd63840729dae96eb53beee7c1ca5da
Dinesh Singh <dinesh@workduck.io>
2022-04-02 22:49:36 +0530

    Exclude Excalidraw element in search

--- src/utils/search/parseData.ts
+++ src/utils/search/parseData.ts
@@ -83,7 +83,6 @@ export const parseNode = (nodeId: string, contents: any[], title = ''): GenericS
       result.push(temp)
     }
   })
-  mog('---------------------- MOGGINNG', { result })
   return result
 }
 
