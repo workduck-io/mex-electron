be85934487c14b28e2c4901165e4e39488a68338
Dinesh Singh <dinesh@workduck.io>
2022-04-05 17:19:44 +0530

    Children in content

--- src/utils/htmlDeserializer.tsx
+++ src/utils/htmlDeserializer.tsx
@@ -12,13 +12,17 @@ import components from '../editor/Components/components'
 import { createPlateUIEditor } from '@udecode/plate'
 import getPlugins from '../editor/Plugins/plugins'
 import { updateIds } from './dataTransform'
-import { Text } from 'slate'
+import { Descendant, Editor, Text } from 'slate'
 import { generateTempId } from '../data/Defaults/idPrefixes'
+import { mog } from './lib/helper'
 
 export const plateEditor = () => {
   const plugins = getPlugins(components, { exclude: { dnd: true } })
   return createPlateUIEditor({ plugins })
 }
+
+const isInlineNode = (editor: Pick<Editor, 'isInline'>) => (node: Descendant) =>
+  Text.isText(node) || editor.isInline(node)
 
 export const getDeserializeSelectionToNodes = (
   selection: { text: string; metadata: string },
@@ -30,8 +34,19 @@ export const getDeserializeSelectionToNodes = (
 
   try {
     nodes = editor ? deserializeHtml(editor, { element, stripWhitespace: true }) : undefined
-    if (nodes && Text.isText(nodes[0])) {
-      nodes = [{ id: generateTempId(), type: ELEMENT_DEFAULT, children: nodes }]
+
+    if (nodes) {
+      let isText = true
+      for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i]
+
+        if (!isInlineNode(editor)(node)) {
+          isText = false
+          break
+        }
+      }
+
+      if (isText) nodes = [{ id: generateTempId(), type: ELEMENT_DEFAULT, children: nodes }]
     }
     if (nodes) nodes = nodes.map((block) => updateIds(block, true))
     if (nodes) nodes = nodes.map((node) => highlightNodes(node, highlight))
