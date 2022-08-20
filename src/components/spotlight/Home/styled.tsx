34d8e7180b842d78c0ff11eb8c246f24f0a8b1b2
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-08-20 19:53:34 +0530

    added pointer moved hook;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- src/components/spotlight/Home/styled.tsx
+++ src/components/spotlight/Home/styled.tsx
@@ -26,20 +26,3 @@ export const ActionItem = styled.div<{ active?: boolean }>`
     `}
   cursor: pointer;
 `
-
-export function usePointerMovedSinceMount() {
-  const [moved, setMoved] = React.useState(false)
-
-  React.useEffect(() => {
-    function handler() {
-      setMoved(true)
-    }
-
-    if (!moved) {
-      window.addEventListener('pointermove', handler)
-      return () => window.removeEventListener('pointermove', handler)
-    }
-  }, [moved])
-
-  return moved
-}
