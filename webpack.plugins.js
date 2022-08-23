9421b294001a972f8fcd908c97e380dec709d1d7
Mukul Mehta <mukul@workduck.io>
2022-08-23 22:35:25 +0530

    Remove webpack and forge scripts and config

--- webpack.plugins.js
+++
@@ -1,6 +0,0 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-// const ThreadsPlugin = require('threads-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-
-module.exports = [new ForkTsCheckerWebpackPlugin(), new webpack.ExternalsPlugin('commonjs', ['electron', 'net'])]
