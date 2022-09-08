2d7c53b266dac227d3c8c2eff4270441328ebad1
Dinesh Singh <dinesh@workduck.io>
2022-09-08 19:46:09 +0530

    Preview Editor replaced with Editor

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
-import EditorPreviewRenderer from '@editor/EditorPreviewRenderer'
 import { useContentStore } from '@store/useContentStore'
+import Editor from '@editor/Editor'
+import { getDefaultContent } from '@components/spotlight/Preview'
+import { useBufferStore, useEditorBuffer } from '@hooks/useEditorBuffer'
 
 const Note: React.FC<{ noteId: string }> = ({ noteId }) => {
   const getContent = useContentStore((store) => store.getContent)
   const noteContent = getContent(noteId)
 
+  const addOrUpdateValBuffer = useBufferStore(store => store.add)
+  const { saveAndClearBuffer } = useEditorBuffer()
+
+  const onChangeSave = useCallback(
+    async (val: any[]) => {
+      if (val && noteId !== '__null__') {
+        addOrUpdateValBuffer(noteId, val)
+      }
+    },
+    [noteId]
+  )
+
+  const onAutoSave = useCallback((val) => {
+    saveAndClearBuffer(false)
+  }, [])
+
   if (noteContent?.content) {
-    return <EditorPreviewRenderer content={noteContent?.content} readOnly={true} editorId={`${noteId}_pinned_note`} />
+    return <Editor
+      showBalloonToolbar
+      onAutoSave={onAutoSave}
+      onChange={onChangeSave}
+      content={noteContent.content.length ? noteContent?.content : getDefaultContent()}
+      editorId={`${noteId}-Pinned-Note`}
+    />
+
   }
 }
 
