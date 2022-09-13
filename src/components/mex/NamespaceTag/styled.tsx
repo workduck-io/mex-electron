da58ec4d207c6b66c4e3c87aea54f53af19003f6
xypnox <xypnox@gmail.com>
2022-09-13 19:27:38 +0530

    Show tags for ns, fix refactor from/to selection

--- src/components/mex/NamespaceTag/styled.tsx
+++ src/components/mex/NamespaceTag/styled.tsx
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import { IconWrapper } from '@ui/components/IconPicker/IconPicker.style'
-import { Ellipsis } from '@workduck-io/mex-components'
 import styled, { css } from 'styled-components'
+
+import { Ellipsis } from '@workduck-io/mex-components'
 
 export const NamespaceText = styled.span`
   min-width: 4rem;
@@ -9,36 +9,21 @@ export const NamespaceText = styled.span`
   ${Ellipsis}
 `
 
-=======
-import styled, { css } from 'styled-components'
-
->>>>>>> 672ed844 (activeNamespace in useUserPreferences, and lookup namespace tags)
 export const StyledNamespaceTag = styled.div<{ separator?: boolean }>`
   color: ${({ theme }) => theme.colors.text.disabled};
   font-size: 0.8rem;
   display: flex;
   align-items: center;
   padding-left: 0.25rem;
-<<<<<<< HEAD
   ${Ellipsis}
   flex-shrink: 0;
   gap: ${({ theme }) => theme.spacing.tiny};
-
   ${IconWrapper} {
     flex-shrink: 0;
   }
-
   ${({ theme, separator }) =>
     separator &&
     css`
       border-left: 1px solid ${theme.colors.gray[7]};
     `}
 `
-=======
-  margin-left: 0.5rem;
-  ${({ theme, separator }) => separator && css`
-    border-left: 1px solid ${theme.colors.text.disabled};
-  `
-  }
-` 
->>>>>>> 672ed844 (activeNamespace in useUserPreferences, and lookup namespace tags)
