dc03f9b2e5a63b1cf4edddf0fed73990bd8ef501
xypnox <xypnox@gmail.com>
2022-09-14 14:19:38 +0530

    Fix refactor to from default values

--- src/editor/Components/combobox/components/PreviewMeta.tsx
+++ src/editor/Components/combobox/components/PreviewMeta.tsx
@@ -1,8 +1,11 @@
+import React, { useMemo } from 'react'
+
 import NamespaceTag from '@components/mex/NamespaceTag'
 import { useNamespaces } from '@hooks/useNamespaces'
-import { mog } from '@workduck-io/mex-utils'
-import React, { useMemo } from 'react'
 import styled from 'styled-components'
+
+import { mog } from '@workduck-io/mex-utils'
+
 import { Data } from '../../../../components/mex/Metadata/Metadata'
 import { RelativeTime } from '../../../../components/mex/RelativeTime'
 
@@ -27,19 +30,12 @@ const PreviewMetaContainer = styled.div`
 `
 
 const PreviewMeta: React.FC<PreviewMetaProps> = ({ meta, namespace }) => {
-<<<<<<< HEAD
   const { getNamespace } = useNamespaces()
   const currentNoteNamespace = useMemo(() => getNamespace(namespace), [namespace])
 
   mog('NAMESPACE', { namespace })
 
-=======
->>>>>>> 672ed844 (activeNamespace in useUserPreferences, and lookup namespace tags)
   if (!meta) return <></>
-  const { getNamespace } = useNamespaces()
-  const currentNoteNamespace = useMemo(() => getNamespace(namespace), [namespace])
-
-  mog("NAMESPACE", { namespace })
 
   return (
     <PreviewMetaContainer>
