8410a2ddbb9f03e0f230ebede0de158d66e335ec
xypnox <xypnox@gmail.com>
2022-09-17 17:00:54 +0530

    Update ILinks when namespace is different (For archived links)

--- src/utils/hierarchy.ts
+++ src/utils/hierarchy.ts
@@ -1,7 +1,7 @@
 import { differenceWith } from 'lodash'
 import { ILink } from '../types/Types'
 
-const compareIlinks = (a: ILink, b: ILink) => a.nodeid === b.nodeid
+const compareIlinks = (a: ILink, b: ILink) => a.nodeid === b.nodeid && a.namespace === b.namespace
 
 export const iLinksToUpdate = (oldHierarchy: ILink[], updatedHierarchy: ILink[]) => {
   const toUpdateLocal = differenceWith(updatedHierarchy, oldHierarchy, compareIlinks)
