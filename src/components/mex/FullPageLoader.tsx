aaf26ffbb072adf16aae9a326e2b42bc6159b16e
Dinesh Singh <dinesh@workduck.io>
2022-09-19 16:02:09 +0530

    Init loading background fix

--- src/components/mex/FullPageLoader.tsx
+++ src/components/mex/FullPageLoader.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ModalOverlay, ModalStyles } from './Refactor/styles'
 import Lottie from 'lottie-react'
 import { loader } from '@components/spotlight/ActionStage/Performers/loader'
 
 const OverlayLoader = styled.div`
-  ${ModalOverlay}
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
   height: 100%;
   width: 100%;
   display: flex;
